73308d947aacdddf07d923229c633a9c09937256
KimMoggiun <wanderingkmg@naver.com>
2022-07-03 00:02:24 +0900

    Initial commit

---
+++ src/test/java/com/springBoot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.springBoot.web;
+
+import junit.framework.TestCase;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest extends TestCase {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+}
